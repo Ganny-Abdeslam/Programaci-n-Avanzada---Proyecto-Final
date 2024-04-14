fd5a2860d2f5c281238e4a9454eb1496944f8661
Ganny <gannyser@gmail.com>
2024-04-14 12:54:09 -0500

    Implementacion filtrar estado negocio

--- src/main/java/co/edu/uniquindio/servicios/interfaces/NegocioServicio.java
+++ src/main/java/co/edu/uniquindio/servicios/interfaces/NegocioServicio.java
@@ -1,13 +1,17 @@
 package co.edu.uniquindio.servicios.interfaces;
 
+import co.edu.uniquindio.dto.CambioEstadoDTO;
 import co.edu.uniquindio.dto.RegistrarNegocioDTO;
+import co.edu.uniquindio.modelos.documentos.Negocio;
+
+import java.util.List;
 
 public interface NegocioServicio {
     void crearNegocio(RegistrarNegocioDTO registrarNegocioDTO) throws Exception;
     void actualizarNegocio();
     void buscarNegocios();
     void eliminarNegocio();
-    void filtrarPorEstado();
+    List<Negocio> filtrarPorEstado(String estado);
     void listarNegociosPropietario();
-    void cambiarEstado();
+    void cambiarEstado(CambioEstadoDTO cambioEstadoDTO) ;
 }
