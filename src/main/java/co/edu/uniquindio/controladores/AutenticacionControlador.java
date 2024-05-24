89fce167923ddbbfb9f0be2d01aced1f6c59cfb8
Ganny <62664788+Ganny-Abdeslam@users.noreply.github.com>
2024-05-24 16:50:49 -0500

    Update AutenticacionControlador.java

--- src/main/java/co/edu/uniquindio/controladores/AutenticacionControlador.java
+++ src/main/java/co/edu/uniquindio/controladores/AutenticacionControlador.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:8082")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AutenticacionControlador {
     private final AutenticacionImplementacion autenticacionImplementacion;
     @PostMapping("/login-cliente")
